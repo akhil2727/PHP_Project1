2355d5a181b15cb1d4b4f8d47e161c68be070b84
akhil2727 <60076014+akhil2727@users.noreply.github.com>
2021-03-03 23:38:42 -0500

    Add files via upload

---
+++ common/footer.php
@@ -0,0 +1,29 @@
+<footer class="footer">
+    <div class="container">
+        <h5>Tatineni Bookstore</h5>
+        <ul class="list-unstyled text-small">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="inventory.php">Inventory</a></li>
+        </ul>
+    </div>
+</footer>
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<script>
+    function setCookie(cname, cvalue, exdays) {
+        var d = new Date();
+        d.setTime(d.getTime() + (exdays * 60 * 60 * 1000));
+        var expires = "expires=" + d.toUTCString();
+        document.cookie = cname + "=" + cvalue + "; " + expires;
+    }
+
+    function showCookie() {
+        document.write(document.cookie);
+    }
+</script>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+include('common/header.php');
+?>
+<div class="container text-center mt-5">
+    <h1>Welcome to Tatineni BookStore</h1>
+    <p>Checkout out wide range of inventory <a href="inventory.php" class="">here</a></p>
+</div>
+
+
+<?php
+include('common/footer.php');
+?>
